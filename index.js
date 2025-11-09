6b066e0e41614f524e05d8b966ec133b01ba6cd1
Rummaninfo <birsabbir38@gmail.com>
2025-11-09 10:09:56 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+let cors = require("cors");
+const port = process.env.PORT || 9000;
+app.use(cors());
+app.use(express.json());
+require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@crud.p5kddzk.mongodb.net/?appName=CRUD`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.get("/load", (req, res) => {
+  res.send("hello i am here");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
+
+
+
+async function run() {
+  try {
+   
+
+   let db = client.db("KrishiLink")
+   let dbCollection = db.collection('allCrops')
+
+
+   app.get("/allcrops", async(req, res)=>{
+     
+     let result = await dbCollection.find().toArray()
+     res.send(result)
+   })
+
+
+
+
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("mongodb connectttttttttttttttt");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
