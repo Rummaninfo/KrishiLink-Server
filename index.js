270ce3c80cf1e851cfe66841589b626e64acc2e1
Rummaninfo <birsabbir38@gmail.com>
2025-11-10 06:48:17 -0600

    my post

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ let cors = require("cors");
 const port = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
-require("dotenv").config()
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@crud.p5kddzk.mongodb.net/?appName=CRUD`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@crud.p5kddzk.mongodb.net/?appName=CRUD`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -27,46 +26,47 @@ app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
 
-
-
 async function run() {
   try {
-   
+    let db = client.db("KrishiLink");
+    let dbCollection = db.collection("allCrops");
 
-   let db = client.db("KrishiLink")
-   let dbCollection = db.collection('allCrops')
+    app.get("/allcrops", async (req, res) => {
+      let result = await dbCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.get("/allcrops/:id", async (req, res) => {
+      let id = req.params.id;
+      let objectId = new ObjectId(id);
+      let result = await dbCollection.findOne({ _id: objectId });
+      res.send(result);
+    });
 
-   app.get("/allcrops", async(req, res)=>{
-     
-     let result = await dbCollection.find().toArray()
-     res.send(result)
-   })
+    // add crop
+    app.post("/allcrops", async (req, res) => {
+      let data = req.body;
+      console.log(data);
+      let result = await dbCollection.insertOne(data);
+      res.send({ success: true });
+    });
 
-   app.get("/allcrops/:id", async(req, res)=>{
-      let id = req.params.id
-      let objectId = new ObjectId(id)
-      let result = await dbCollection.findOne({_id:objectId})
-      res.send(result)
-   })
-
-   // add crop
-    app.post("/allcrops", async(req, res)=>{
-     let data = req.body
-     console.log(data)
-      let result = dbCollection.insertOne(data)
-      res.send({success: true})
-
-    })
-
-
-
+    app.get("/myposts", async (req, res) => {
+      let email = req.query.email;
+      if (!email) {
+        return res.status(400).send({ message: "email query required" });
+      }
+      let rows = await dbCollection
+        .find({ "owner.ownerEmail": email })
+        .sort({ createdAt: -1 })
+        .toArray()
+        res.send(rows)
+    });
 
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log("mongodb connectttttttttttttttt");
   } finally {
-    
   }
 }
 run().catch(console.dir);
